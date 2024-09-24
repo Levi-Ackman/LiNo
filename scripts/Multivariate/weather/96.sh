6ed8e6a3fc1d38c579aa663ce31a7bb4d34c83e1
Yu Guoqi <108868447+Levi-Ackman@users.noreply.github.com>
2024-09-24 14:04:19 +0800

    Add files via upload

---
+++ scripts/Multivariate/weather/96.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+mkdir -p ./logs/LongForecasting/weather
+
+export CUDA_VISIBLE_DEVICES=0
+model_name=LiNo
+seq_lens=(96)
+bss=(256)
+lrs=(1e-4)
+log_dir="./logs/LongForecasting/weather/"
+layers=(4)
+pred_lens=(96)
+dropouts=(0.2)
+d_models=(512)
+
+for bs in "${bss[@]}"; do
+    for lr in "${lrs[@]}"; do
+        for layer in "${layers[@]}"; do
+            for dropout in "${dropouts[@]}"; do
+                for d_model in "${d_models[@]}"; do
+                    for pred_len in "${pred_lens[@]}"; do
+                        for seq_len in "${seq_lens[@]}"; do
+                            python -u run.py \
+                            --task_name long_term_forecast \
+                            --is_training 1 \
+                            --root_path ./dataset/weather/ \
+                            --data_path weather.csv \
+                            --model_id "weather_${seq_len}_${pred_len}" \
+                            --model $model_name \
+                            --data custom \
+                            --features M \
+                            --seq_len $seq_len \
+                            --pred_len $pred_len \
+                            --batch_size $bs\
+                            --learning_rate $lr \
+                            --layers $layer\
+                            --dropout $dropout\
+                            --d_model $d_model\
+                            --enc_in 21 \
+                            --dec_in 21 \
+                            --c_out 21 \
+                            --des 'Exp' \
+                            --patience 6\
+                            --itr 1 >"${log_dir}bs${bs}_lr${lr}_lay${layer}_dp${dropout}_dm${d_model}_${pred_len}_${seq_len}.log"
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
---
+++ scripts/Univariate/traffic/192.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+mkdir -p ./logs/LongForecasting/traffic
+
+export CUDA_VISIBLE_DEVICES=4
+model_name=LiNo
+seq_lens=(96)
+bss=(128)
+lrs=(1e-3)
+log_dir="./logs/LongForecasting/traffic/"
+layers=(3)
+pred_lens=(192)
+dropouts=(0.2)
+d_models=(512)
+
+for bs in "${bss[@]}"; do
+    for lr in "${lrs[@]}"; do
+        for layer in "${layers[@]}"; do
+            for dropout in "${dropouts[@]}"; do
+                for d_model in "${d_models[@]}"; do
+                    for pred_len in "${pred_lens[@]}"; do
+                        for seq_len in "${seq_lens[@]}"; do
+                            python -u run.py \
+                            --task_name long_term_forecast \
+                            --is_training 1 \
+                            --root_path /data/gqyu/dataset/traffic/ \
+                            --data_path traffic.csv \
+                            --model_id "traffic_${seq_len}_${pred_len}" \
+                            --model $model_name \
+                            --data custom \
+                            --features S \
+                            --seq_len $seq_len \
+                            --pred_len $pred_len \
+                            --batch_size $bs \
+                            --learning_rate $lr \
+                            --layers $layer\
+                            --dropout $dropout\
+                            --d_model $d_model\
+                            --enc_in 1 \
+                            --dec_in 1 \
+                            --c_out 1 \
+                            --des 'Exp' \
+                            --patience 6\
+                            --itr 1 >"${log_dir}bs${bs}_lr${lr}_lay${layer}_dp${dropout}_dm${d_model}_${pred_len}_${seq_len}.log"
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
+
